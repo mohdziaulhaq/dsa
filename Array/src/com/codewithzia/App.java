eb650a3b71ed3018af01f8ad8fc296febe2a8928
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2021-01-03 21:43:38 +0530

    Arrays

---
+++ Array/src/com/codewithzia/App.java
@@ -0,0 +1,16 @@
+package com.codewithzia;
+
+public class App {
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(50);
+        // numbers.insert(10);
+        // numbers.removeAt(3);
+        // numbers.indexOf(100);
+        numbers.print();
+    }
+}
---
+++ Array/src/com/codewithzia/Array.java
@@ -0,0 +1,34 @@
+package com.codewithzia;
+public class Array {
+    private int[] items;
+    private int count = 0;
+
+    public Array(int length){
+        items = new int[length];
+    }
+
+    public void insert(int item){
+        // if the array is full, resize it
+        if(items.length == count){
+            //create a new array (twice the size)
+            int[] newItems = new int[count*2];
+
+            //copy all the existing items
+            for(int i = 0; i < count; i++){
+                newItems[i] = items[i];
+
+                //set "items" to this new array
+                items = newItems;
+            }
+        }
+
+        //add the new item at the end
+        items[count++] = item;
+    }
+
+    public void print(){
+        for(int i = 0; i < count; i++){
+            System.out.println(items[i]);
+        }
+    }
+}
