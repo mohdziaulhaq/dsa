3bd770a3685b75d33d4c9ec676909ecaa6dfd193
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2021-01-22 20:20:21 +0530

    Implemented Stack using Arrays

--- Stack/src/com/codewithzia/App.java
+++ Stack/src/com/codewithzia/App.java
@@ -31,10 +31,22 @@ public class App {
         // System.out.println(result);
 
         //Multiple braces
-        String str = "(1 + 2}";
-        Expression exp = new Expression();
-        var result = exp.isBalanced(str);
-        System.out.println(result);
+        // String str = "({<1> + 2})";
+        // Expression exp = new Expression();
+        // var result = exp.isBalanced(str);
+        // System.out.println(result);
         
+        //Stack implementation using Array
+        //[10,20,0,0,0]
+        //count = 2
+        //push(20)
+        //array[count] = 20;
+        com.codewithzia.Stack stack = new com.codewithzia.Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack);
     }
 }
